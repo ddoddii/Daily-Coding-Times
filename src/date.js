2cd578603feaddcb84e881c988041e6164ed61c4
Soeun Uhm <uhmturks@yonsei.ac.kr>
2024-07-01 01:56:33 +0900

    chore : add log to for date updates

--- src/date.js
+++ src/date.js
@@ -1,5 +1,6 @@
 export function getCurrentDate() {
     const today = new Date();
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    console.log("✅ : today's date updated : ", today.toLocaleDateString('en-US', options))
     return today.toLocaleDateString('en-US', options);
 }
