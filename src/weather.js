f6417b646f8b30e44d1ae702ec7b62caffde4b4c
Soeun Uhm <uhmturks@yonsei.ac.kr>
2024-07-12 01:10:54 +0900

    feat : add moon face at night

--- src/weather.js
+++ src/weather.js
@@ -4,6 +4,7 @@ import { fileURLToPath } from 'url';
 import { dirname,join } from 'path';
 import config from '../config.js';
 
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -18,7 +19,8 @@ const weatherImages = {
     Rain: 'images/weather/Umbrella with Rain Drops.png',
     Snow: 'images/weather/Cloud with Snow.png',
     Fog: 'images/weather/Fog.png',
-	Mist: 'images/weather/Fog.png'
+	Mist: 'images/weather/Fog.png',
+	Moon : 'images/weather/Full Moon Face.png',
 };
 
 const timeImages = {
@@ -26,22 +28,39 @@ const timeImages = {
 	SunSet : 'images/weather/Sunset.png',
 };
 
-async function getCurrentWeather() {
-	const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather`, {
-		params: {
-			lat: lat,
-			lon: lon,
-			appid: config.OpenWeatherApiKey,
-		}
-	});
-	const currentWeather = res.data['weather'][0]['main'];
-	const currentWeatherDescription = res.data['weather'][0]['description'];
-	const tempInKelvin = res.data['main']['temp'];
-	const tempInCelcius = Math.round(tempInKelvin - 273);
-	const weatherImagePath = weatherImages[currentWeather] || 'images/weather/Star.png';
-    console.log("✅ : Daily Weather updated successfully -> ", tempInCelcius, currentWeather, currentWeatherDescription);
-	return {currentWeather, currentWeatherDescription, tempInCelcius, weatherImagePath};
+function isNight(currentHour) {
+	return (currentHour < 5 || currentHour > 20);
 }
+
+async function getCurrentWeather() {
+	try {
+		const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather`, {
+			params: {
+				lat: lat,
+				lon: lon,
+				appid: config.OpenWeatherApiKey,
+			}});
+		const currentWeather = res.data['weather'][0]['main'];
+		const currentWeatherDescription = res.data['weather'][0]['description'];
+		const tempInKelvin = res.data['main']['temp'];
+		const tempInCelcius = Math.round(tempInKelvin - 273);
+		// weather Image path
+		let weatherImagePath;
+		const currentHour = new Date().getHours();
+		if (isNight(currentHour) && currentWeather === 'Clear') {
+			weatherImagePath = weatherImages['Moon'];
+		} else {
+			weatherImagePath = weatherImages[currentWeather] || 'images/weather/Star.png';
+		}
+		console.log("✅ : Daily Weather updated successfully -> ", tempInCelcius, currentWeather, currentWeatherDescription);
+		return {currentWeather, currentWeatherDescription, tempInCelcius, weatherImagePath};
+
+		} catch (error) {
+				console.error("@src/trend.js : Failed to fetch trend data from Firestore:", error);
+		}
+
+	}
+
 
 
 export {getCurrentWeather};
